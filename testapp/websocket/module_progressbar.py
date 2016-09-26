c952c87a14467604dddc08119998f1fb79a055c9
Hugo ALLIAUME <kocal@live.fr>
2016-09-26 10:47:18 +0200

    testapp: cleaning code by using Tornado asynchronous methods

--- testapp/websocket/module_progressbar.py
+++ testapp/websocket/module_progressbar.py
@@ -2,33 +2,30 @@
     Example of module « Progress Bar » by using `tornado_websocket.modules.ProgressBar` to handle communications,
     and Django's TemplateView for rendering.
 """
-import threading
-import time
 
 from django.views.generic import TemplateView
+from tornado import gen
 
 from tornado_websockets.modules import ProgressBar
 from tornado_websockets.websocket import WebSocket
 
 tws = WebSocket('module_progressbar')
-progress_bar = ProgressBar('foo', min=0, max=200)
+progress_bar = ProgressBar('foo', min=0, max=100)
 
 tws.bind(progress_bar)
 
 
 @progress_bar.on
-def start():
+def reset():
     progress_bar.reset()
-    start_thread(progress_bar)
 
 
-def start_thread(bb):
-    def fn():
-        for value in range(0, progress_bar.max):
-            time.sleep(.1)
-            progress_bar.tick(label="[%d/%d] Tâche %d terminée" % (progress_bar.value + 1, progress_bar.max, value))
-
-    threading.Thread(None, fn, None).start()
+@progress_bar.on
+@gen.engine  # Asynchronous for Tornado's IOLoop
+def start():
+    for value in range(0, progress_bar.max):
+        yield gen.sleep(.1)  # like time.sleep(), but asynchronous
+        progress_bar.tick(label="[%d/%d] Tâche %d terminée" % (progress_bar.current + 1, progress_bar.max, value))
 
 
 class MyProgressBar(TemplateView):
