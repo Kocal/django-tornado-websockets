027d670e6fd28356291b3145fea85d37171df729
Hugo Alliaume <kocal@live.fr>
2016-10-09 21:58:43 +0200

    Tests/runtornado: add tests for handlers and settings

--- tornado_websockets/tests/test_command_runtornado.py
+++ tornado_websockets/tests/test_command_runtornado.py
@@ -15,7 +15,7 @@ django.setup()
 
 class TestCommandRuntornado(TestCase):
     """
-    Tests for the management command « runtornado ».
+        Tests for the management command « runtornado ».
     """
 
     def setUp(self):
@@ -27,14 +27,27 @@ class TestCommandRuntornado(TestCase):
     def tearDown(self):
         settings.TORNADO = self.TORNADO
 
+    '''
+        Tests for settings behavior.
+    '''
+
     def test_without_tornado_settings(self):
         del settings.TORNADO
 
-        out = StringIO()
         err = StringIO()
-        call_command('runtornado', stdout=out, stderr=err)
+        call_command('runtornado', stdout=StringIO(), stderr=err)
 
         self.assertIn("Configuration => Not found: 'Settings' object has no attribute 'TORNADO'", err.getvalue())
+
+    def test_with_tornado_settings(self):
+        err = StringIO()
+        call_command('runtornado', stdout=StringIO(), stderr=err)
+
+        self.assertNotIn("Configuration => Not found: 'Settings' object has no attribute 'TORNADO'", err.getvalue())
+
+    '''
+        Tests for port behavior.
+    '''
 
     def test_get_port_from_options(self):
         settings.TORNADO['port'] = 1234
@@ -62,3 +75,39 @@ class TestCommandRuntornado(TestCase):
 
         self.assertIn('Port => 8000', out.getvalue())
         TornadoWrapper.listen.assert_called_with(8000)
+
+    '''
+        Tests for handlers behavior.
+    '''
+
+    def test_without_handlers(self):
+        del settings.TORNADO['handlers']
+
+        out = StringIO()
+        call_command('runtornado', stdout=out)
+
+        self.assertIn('Handlers => Found 0 initial handlers.', out.getvalue())
+
+    def test_with_handlers(self):
+        out = StringIO()
+        call_command('runtornado', stdout=out)
+
+        self.assertIn('Handlers => Found 2 initial handlers.', out.getvalue())
+
+    '''
+        Tests for settings behavior.
+    '''
+
+    def test_without_settings(self):
+        del settings.TORNADO['settings']
+
+        out = StringIO()
+        call_command('runtornado', stdout=out)
+
+        self.assertIn('Settings => {}', out.getvalue())
+
+    def test_with_settings(self):
+        out = StringIO()
+        call_command('runtornado', stdout=out)
+
+        self.assertIn('Settings => {"autoreload": true, "debug": true}', out.getvalue())
